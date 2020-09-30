1b9354078d40a3b95ed0058e3a18ceefac157a9f
Anber Jamshed <anberjam@yahoo.com>
2020-09-30 20:59:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.detect do |element|
-    element == "Earth!" || element == "Wind!" || element == "Fire!" || element == "Water!" || element == "Heart!"
+
   end
 end
